7fd2026db7aee96ee2b6f2429e7a531517779025
Jimmy <ijimmywei@gmail.com>
2020-06-11 11:28:42 +0100

    Implements TypeScript support

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+import { ViewStyle } from 'react-native';
+
+export interface OnChangeProps {
+    base64URL: string
+}
+
+export interface OnErrorProps {
+    details: string
+}
+
+export interface SignaturePadProps {
+    onChange: (props: OnChangeProps) => void;
+    onError: (props: OnErrorProps) => void;
+    style?: ViewStyle;
+    penColor?: string;
+    dataURL?: string;
+}
+
+export default SignaturePad;
